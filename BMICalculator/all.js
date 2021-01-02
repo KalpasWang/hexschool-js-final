db2034b32204ceabce8fcbe3c70b66af246e3694
KalpasWang <48082968+KalpasWang@users.noreply.github.com>
2021-01-02 15:09:25 +0800

    BMI Calculator 程式碼加上註解

--- BMICalculator/all.js
+++ BMICalculator/all.js
@@ -1,26 +1,34 @@
+// 處理 local storage 的讀取
 class Storage {
   constructor() {
     this.loadData();
   }
+  // 讀取 local storage 的歷史資料
   loadData() {
     const str = localStorage.getItem('bmiData');
     const localData = JSON.parse(str);
     this.data = !localData ? [] : localData;
   }
+  // 回傳現有資料的拷貝
   getDate() {
-    return this.data;
+    return this.data.slice(0);
   }
+  // 儲存資料
   storeData(newData) {
     this.data.unshift(newData);
     localStorage.setItem('bmiData', JSON.stringify(this.data));
   }
+  // 清除所有資料
   clearData() {
     this.data = [];
     localStorage.clear();
   }
 }
 
+// 處理畫面元件的顯示以及事件的綁定、特效等
 class UI {
+  // 建立的時候取得所有所需的 DOM 元素
+  // 並將沒有需要外部資訊的元素綁定事件
   constructor() {
     this.height = document.getElementById('height');
     this.weight = document.getElementById('weight');
@@ -31,22 +39,29 @@ class UI {
     this.refresh = document.getElementById('refresh');
     this.addRefreshEvent();
   }
+  // 將歷史資料顯示在列表
   showOldData(oldData) {
     if (!oldData || !oldData.length) return;
     oldData.forEach((item) => {
       this.bmiHistory.insertAdjacentHTML(
         'beforeend',
+        // 後面的 true 表示這是舊資料
         this.getTemplate(item, true)
       );
     });
   }
+  // 將計算出來的 BMI 與 身體肥胖狀態渲染到畫面上
   renderResult(bmi, { statusZh, statusEn }) {
     this.summary.querySelector('.status').className = `status ${statusEn}`;
     this.summary.querySelector('.status-circle-title').textContent = `${bmi}`;
     this.summary.querySelector('.status-title').textContent = `${statusZh}`;
+
+    // 切換顯示結果
     this.fadeIn(this.summary);
     this.fadeOut(this.getResult);
+    this.refresh.setAttribute('tabindex', '0');
 
+    // 取得要渲染到歷史紀錄的資料並且套用模板顯示在畫面上
     const today = Utils.getToday();
     const weight = this.weight.value;
     const height = this.height.value;
@@ -59,6 +74,7 @@ class UI {
       today,
     };
     this.bmiHistory.insertAdjacentHTML('afterbegin', this.getTemplate(bmiData));
+    // 加上特效
     this.setListItemAnimation('li:first-child');
     return bmiData;
   }
@@ -68,6 +84,7 @@ class UI {
       height: this.height.value,
     };
   }
+  // 歷史紀錄樣板，如果沒有 class 沒有加上 show 會先隱藏
   getTemplate(bmiData, isOld) {
     const show = isOld ? 'show ' : '';
     return `<li class="${show}${bmiData.statusEn}">
@@ -78,19 +95,23 @@ class UI {
               <div class="cell date"><small>${bmiData.today}</small></div> 
             </li>`;
   }
+  // 處理當按下要看結果的按鈕的事件
   addGetResultEvent(handler) {
     this.getResult.addEventListener('click', handler);
   }
+  // 處理當按下要清除歷史紀錄的按鈕的事件
   addClearHistoryEvent(storage) {
     this.clear.addEventListener('click', () => {
       storage.clearData();
       this.bmiHistory.innerHTML = '';
     });
   }
+  // 處理當按下要從結果返回的按鈕的事件
   addRefreshEvent() {
     const handler = () => {
       this.fadeOut(this.summary);
       this.fadeIn(this.getResult);
+      this.refresh.setAttribute('tabindex', '-1');
       this.height.value = '';
       this.weight.value = '';
     };
@@ -101,6 +122,7 @@ class UI {
       }
     });
   }
+  // 淡出特效
   fadeOut(el) {
     el.classList.add('hidden');
     el.addEventListener(
@@ -111,34 +133,41 @@ class UI {
       { once: true }
     );
   }
+  // 淡入特效
   fadeIn(el) {
     el.style.visibility = 'visible';
     el.classList.remove('hidden');
   }
+  // 新的結果加入歷史紀錄的特效
   setListItemAnimation(selector) {
     const li = document.querySelector(selector);
     setTimeout(() => {
       li.classList.add('show');
-    }, 500);
+    }, 1000);
   }
 }
 
+// 處理邏輯運算、日期換算等
 class Utils {
+  // 計算 BMI
   static calculateBmi(weight, height) {
     const w = parseInt(weight);
     const h = parseInt(height) / 100;
 
+    // 如果傳入的不是數字或小於0，就回傳 false
     if (!w || !h || w <= 0 || h <= 0) {
       return false;
     }
     const bmi = (w / (h * h)).toFixed(2);
 
+    // 如果算出來的 BMI 小於 0，或是是 infinity 或 NaN，回傳 false
     if (bmi <= 0 || !isFinite(bmi) || isNaN(bmi)) {
       return false;
     } else {
       return bmi;
     }
   }
+  // 取得格式化的今天的日期：例如 12-31-2020
   static getToday() {
     const d = new Date();
     const year = d.getFullYear();
@@ -147,6 +176,7 @@ class Utils {
     const day = d.getDate();
     return `${monthStr}-${day}-${year}`;
   }
+  // 確認 BMI 值對應的狀態與 class
   static checkBmiStatus(bmi) {
     let status;
     const statusMap = {
@@ -170,14 +200,18 @@ class Utils {
   }
 }
 
+// 初始化
 function init() {
   const storage = new Storage();
   const ui = new UI();
 
+  // 顯示過去的 BMI 歷史資料
   ui.showOldData(storage.getDate());
+  // 當使用者按下「看結果」的按鈕，處理使用者的身高體重數據
   ui.addGetResultEvent(function () {
     const { weight, height } = ui.getUserData();
     const bmi = Utils.calculateBmi(weight, height);
+    // 如果得到的 BMI 有問題(false)，則 return
     if (!bmi) {
       alert('數值錯誤，請重新輸入');
       return;
@@ -186,6 +220,7 @@ function init() {
     const newData = ui.renderResult(bmi, status);
     storage.storeData(newData);
   });
+  // 綁定「清除歷史」按鈕的事件，傳入 storage 物件實體 讓 ui 可以清除歷史
   ui.addClearHistoryEvent(storage);
 }
 
